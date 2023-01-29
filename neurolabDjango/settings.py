e2e956f42fa601dfe61d6c3a8c250483c0ddc8b7
Atul Kumar Singh <atul0003214@gmail.com>
2023-01-29 14:48:39 +0530

    25

--- neurolabDjango/settings.py
+++ neurolabDjango/settings.py
@@ -80,10 +80,10 @@ ROOT_URLCONF = 'neurolabDjango.urls'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
